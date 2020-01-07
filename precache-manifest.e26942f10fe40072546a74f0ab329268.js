4cc99c2f2acad2809ae57a669abad8915dd2ff9b
Michalina Gańczarczyk <michalina.ganczarczyk@gmail.com>
2020-01-07 23:58:59 +0100

    Updates

---
+++ precache-manifest.e26942f10fe40072546a74f0ab329268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5858d39140586d7752e10d09748de894",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "37ba38dbf1ca8c623f85",
+    "url": "/todolist/static/css/main.198fbeac.chunk.css"
+  },
+  {
+    "revision": "be92bfaaeb92ac2246d6",
+    "url": "/todolist/static/js/2.23d4b181.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist/static/js/2.23d4b181.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37ba38dbf1ca8c623f85",
+    "url": "/todolist/static/js/main.311d0f5a.chunk.js"
+  },
+  {
+    "revision": "666f83b7e93f879b23f9",
+    "url": "/todolist/static/js/runtime-main.4fea2076.js"
+  }
+]);
